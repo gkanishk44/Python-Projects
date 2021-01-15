debe4e9693d18367daa64974931e4a36ea6ad441
KANISHK KUMAR GUPTA <kanishk2019cs159@abesit.edu.in>
2021-01-15 09:31:53 +0530

    cloning or copying a list

---
+++ cloning a list.py
@@ -0,0 +1,11 @@
+
+def Cloning(list1): 
+	li_copy = list1[:] 
+	return list_copy 
+ 
+list1 = [4, 8, 2, 10, 15, 18] 
+list2 = Cloning(li1) 
+print("Original List:", list1) 
+print("After Cloning:", list2) 
+
+#this is How we can clone a list(contributed by kanishk)
