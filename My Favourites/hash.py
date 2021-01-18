4f56e032145ad44c91dd84a41b7a0e1886713e0a
KANISHK KUMAR GUPTA <kanishk2019cs159@abesit.edu.in>
2021-01-18 19:57:49 +0530

    finding Hash

---
+++ My Favourites/hash.py
@@ -0,0 +1,20 @@
+import hashlib
+
+def hash_file(filename):
+   """"This function returns the SHA-1 hash
+   of the file passed into it"""
+   
+   h = hashlib.sha1()
+
+   with open(filename,'rb') as file:
+
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           h.update(chunk)
+           
+   return h.hexdigest()
+
+message = hash_file("track1.mp3")
+print(message)
