684d013b1bd8a45fd0d6298bea3aca5aea54ab31
KANISHK KUMAR GUPTA <kanishk2019cs159@abesit.edu.in>
2021-01-22 20:07:22 +0530

    collection.deque

---
+++ stackimplementation.py
@@ -0,0 +1,18 @@
+from collections import deque
+ 
+stack = deque()
+ 
+stack.append('a')
+stack.append('b')
+stack.append('c')
+ 
+print('Initial stack:')
+print(stack)
+ 
+print('\nElements poped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+ 
+print('\nStack after elements are poped:')
+print(stack)
